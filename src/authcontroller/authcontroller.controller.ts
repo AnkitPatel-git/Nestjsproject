01295726f11f0e91688d56613a7c7122fb296760
AnkitPatel-git <ankitpatelraja9@gmail.com>
2023-10-31 15:42:58 +0530

    31102023:02

--- src/authcontroller/authcontroller.controller.ts
+++ src/authcontroller/authcontroller.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
-@Controller('authcontroller')
+@Controller('auth')
 export class AuthcontrollerController {
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
   @Get()
-  findAll(): string {
+  findAll(@Req() req: Request): string {
+    return 'This action returns all cats';
+  }
+  @Get('a**')
+  findaAll(@Req() req: Request): string {
     return 'This action returns all cats';
   }
 }
